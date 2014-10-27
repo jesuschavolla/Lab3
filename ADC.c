1cd0511d911b9d0543f4e37766ceaf279f8c2ff4
jesuschavolla <jesuschavolla@email.arizona.edu>
2014-10-27 15:48:36 -0700

    adc done

--- ADC.c
+++ ADC.c
@@ -4,9 +4,32 @@
 #include "ADC.h"
 // ******************************************************************************************* //
 
-double AnalogtoDigital(){
+void InitADC(){
 
+    AD1PCFG= 0xFFDF;// select analog input pins//0b1111111111011111 ch5 Jp 5 is connected internally
+    AD1CON1 = 0x00E4;   //111 = Internal counter ends sampling and starts conversion (auto-convert)
+                        //  Auto converting after end of sampling
+    AD1CON2 = 0x0000;
+    AD1CON3 = 0x1F01;   // sample time = 31Tad, Tad = 2*Tcy = 125 ns
 
+    AD1CSSL = 0x0000;        // no scanning
+    AD1CHS = 0x0000;             //Configure input channels,
+
+        AD1CON1bits.ADON = 1; // Turn on A/D
+
+};
+
+int AnalogtoDigital(){//function that returns the digital value
+    int ADC=0;
+
+    AD1CHS = 0x0005;    // select analog input channel-AN5
+    AD1CON1bits.SAMP = 1;// start sampling, automatic conversion will follow
+
+// wait to complete the sampling and then return value
+    if(AD1CON1bits.DONE==1){
+    ADC=ADC1BUF0;
+    return(ADC);
+    }
 
 
 };
